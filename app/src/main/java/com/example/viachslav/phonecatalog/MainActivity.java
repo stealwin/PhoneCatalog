bad98cf8997cfd18132f087917324418f5bf16f3
Viacheslav Sharko <steal1991@yandex.ru>
2018-06-03 20:08:47 +0300

    Preparing for TextView

--- app/src/main/java/com/example/viachslav/phonecatalog/MainActivity.java
+++ app/src/main/java/com/example/viachslav/phonecatalog/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.viachslav.phonecatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,43 +44,64 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
+        // Получим идентификатор ListView
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles));
+        listView.setTextFilterEnabled(true);
+        //Обрабатываем щелчки на элементах ListView:
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, DetailActivity.class);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+                intent.putExtra("title", position);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                //запускаем вторую активность
+                startActivity(intent);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            }
         }
-        switch (id) {
-            case R.id.action_settings:
-                Toast.makeText(MainActivity.this,getString(R.string.action_settings),Toast.LENGTH_LONG).show();
-                break;
-            case R.id.action_search:
-                Toast.makeText(MainActivity.this,getString(R.string.action_search),Toast.LENGTH_LONG).show();
-                break;
-            case R.id.action_favorites:
-                Toast.makeText(MainActivity.this,getString(R.string.action_favorites),Toast.LENGTH_LONG).show();
-                break;
-            case R.id.action_recent:
-            Toast.makeText(MainActivity.this,getString(R.string.action_recent),Toast.LENGTH_LONG).show();
-            break;
-            case R.id.action_item3:
-                Toast.makeText(MainActivity.this,getString(R.string.action_item3),Toast.LENGTH_LONG).show();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
+        );
     }
-}
+
+            @Override
+            public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.menu_main, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                // Handle action bar item clicks here. The action bar will
+                // automatically handle clicks on the Home/Up button, so long
+                // as you specify a parent activity in AndroidManifest.xml.
+                int id = item.getItemId();
+
+                //noinspection SimplifiableIfStatement
+                if (id == R.id.action_settings) {
+                    return true;
+                }
+                switch (id) {
+                    case R.id.action_settings:
+                        Toast.makeText(MainActivity.this, getString(R.string.action_settings), Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.action_search:
+                        Toast.makeText(MainActivity.this, getString(R.string.action_search), Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.action_favorites:
+                        Toast.makeText(MainActivity.this, getString(R.string.action_favorites), Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.action_recent:
+                        Toast.makeText(MainActivity.this, getString(R.string.action_recent), Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.action_item3:
+                        Toast.makeText(MainActivity.this, getString(R.string.action_item3), Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return false;
+            }
+
+        }
+
