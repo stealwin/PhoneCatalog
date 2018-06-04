698966e4f586315d8d649eb4ba11be30bc2a176d
Viacheslav Sharko <steal1991@yandex.ru>
2018-06-04 20:05:38 +0300

    Preparing for TextView

--- app/src/main/java/com/example/viachslav/phonecatalog/MainActivity.java
+++ app/src/main/java/com/example/viachslav/phonecatalog/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 //запускаем вторую активность
                 startActivity(intent);
 
+
             }
         }
         );
